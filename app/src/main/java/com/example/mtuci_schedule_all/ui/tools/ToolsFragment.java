ef2502b1baab637c47ca3fc07ff30d1d0572c2f0
polestvr <polestvr@MacBook-Pro-Grac.local>
2020-02-09 20:39:10 +0300

    bug fixed

--- app/src/main/java/com/example/mtuci_schedule_all/ui/tools/ToolsFragment.java
+++ app/src/main/java/com/example/mtuci_schedule_all/ui/tools/ToolsFragment.java
@@ -10,9 +10,13 @@ import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mtuci_schedule_all.FacultyList;
 import com.example.mtuci_schedule_all.R;
+import com.example.mtuci_schedule_all.TimeTable;
+import com.example.mtuci_schedule_all.TimetableAdapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -28,6 +32,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -130,6 +135,9 @@ public class ToolsFragment extends Fragment {
                 LTRadapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
                 spinnerFaculty.setAdapter(LTRadapter);
 
+                final ArrayList<String> group_ids;
+                group_ids = new ArrayList<String>();
+
                 spinnerFaculty.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                     @Override
                     public void onItemSelected(AdapterView<?> adapterView, View view, final int position, long id) {
@@ -137,17 +145,27 @@ public class ToolsFragment extends Fragment {
                         final String faculty_id = ""+position;
                         final ArrayList<String> group_list;
                         group_list = new ArrayList<String>();
-                        DatabaseReference facultyInfo = database.getReference("faculty_list").child(faculty_id).child("group_list");
-                        facultyInfo.addValueEventListener(new ValueEventListener() {
+
+
+                        final DatabaseReference group_info = database.getReference("faculty_list").child(faculty_id).child("group_list");
+
+                        group_info.addValueEventListener(new ValueEventListener() {
                             @Override
                             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                group_list.clear();
+                                group_ids.clear();
                                 for (@NonNull DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
-                                    group_list.add(dataSnapshot1.getValue().toString());
+                                    System.out.println(dataSnapshot);
+
+                                    group_list.add(dataSnapshot1.child("name").getValue().toString());
+                                    group_ids.add(dataSnapshot1.child("id").getValue().toString());
+
                                 }
 
                                 ArrayAdapter<String> LTRadapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item,group_list);
                                 LTRadapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
                                 spinnerGroups.setAdapter(LTRadapter);
+
                                 File file = new File( "config.json");
 
                                 try {
@@ -156,8 +174,7 @@ public class ToolsFragment extends Fragment {
                                     e.printStackTrace();
                                 }
 
-                                save(faculty_id);
-                                System.out.println(load());
+
 
 
 
@@ -176,6 +193,18 @@ public class ToolsFragment extends Fragment {
 
                     @Override
                     public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+                spinnerGroups.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        save(group_ids.get(position));
+                        System.out.println(load());
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
 
                     }
                 });
